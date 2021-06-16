9bc37021ffe801cb81b15b489b58fea742ce7fd1
murase masaki <murasemasaki_0216@yahoo.co.jp>
2021-06-16 22:45:04 +0900

    daily push 環境変数を使えるようにした

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.7.2
+	github.com/joho/godotenv v1.3.0
 	golang.org/x/tools/gopls v0.7.0 // indirect
 )
--- service/controller/IndexController.go
+++ service/controller/IndexController.go
@@ -2,12 +2,23 @@ package controller
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func IndexIndexAction(c *gin.Context) {
+
+	//環境変数読み込み
+	err := godotenv.Load("./.env_dev")
+	if err != nil {
+		// .env読めなかった場合の処理
+		c.JSON(200, gin.H{"message": err})
+	}
+
 	c.HTML(http.StatusOK, "index.html", gin.H{
-		"message": "index message",
+		"message":  "index message",
+		"message2": os.Getenv("ENV"),
 	})
 }
